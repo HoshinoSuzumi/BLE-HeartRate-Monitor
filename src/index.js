6550f23160a1029bb0b33e846a3f17d6277a4509
HoshinoSuzumi <master@uniiem.com>
2022-09-25 18:44:25 +0800

    能用

--- src/index.js
+++ src/index.js
@@ -1,47 +1,99 @@
+const { ipcRenderer } = require('electron')
+const echarts = require('echarts')
+
+let chart_x = []
+let chart_data = []
+let chart = echarts.init(document.getElementById('charts'))
+chart.setOption({
+    tooltip: {},
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: []
+    },
+    yAxis: {
+        boundaryGap: [0, '50%'],
+        type: 'value'
+    },
+    series: [
+        {
+            data: [0],
+            type: 'line',
+            smooth: true
+        }
+    ]
+});
+window.onresize = function () {
+    chart.resize();
+};
+
 const elemBtnConnectBle = document.getElementById('btn-connect-ble');
+const elemAppTopBar = document.querySelector('.app-top-bar')
+const elemHeartRateValue = document.getElementById('heart-rate-value')
+
+let BleDevice = null;
+let connectionStatus = {
+    status: false,
+    device: null
+}
+
+ipcRenderer.on('push-connect-status', (ev, status) => {
+    connectionStatus = status;
+    elemAppTopBar.innerHTML += ` · [已连接] ${connectionStatus.device.deviceName || connectionStatus.device.deviceId || 'N/A'}`;
+})
 
 elemBtnConnectBle.addEventListener('click', () => {
-    deviceList = []
+    connectionStatus.status = false
     elemBtnConnectBle.disabled = true;
 
-    navigator.bluetooth.requestDevice({
-        filters: [{ services: ['heart_rate'] }]
-    }).then(device => {
-        return device.gatt.connect();
-    }).catch(err => { });
-
     let scan_job = setInterval(() => {
-        if (deviceList && deviceList.length === 0) {
-            navigator.bluetooth.requestDevice({
+        if (!connectionStatus.status) {
+            BleDevice = navigator.bluetooth.requestDevice({
                 filters: [{ services: ['heart_rate'] }]
+            }).then(device => {
+                console.log('> Name:             ' + connectionStatus.device.deviceName);
+                console.log('> Id:               ' + device.id);
+                console.log('> Is connected:     ' + device.gatt.connected);
+                return device.gatt.connect();
             }).catch(err => { });
             next = false;
         } else {
             elemBtnConnectBle.disabled = false;
+            elemBtnConnectBle.classList.add('connected');
             clearInterval(scan_job);
             scan_finish();
         }
     }, 500);
 
     const scan_finish = () => {
-        navigator.bluetooth.
-        console.log(deviceList);
+        BleDevice.then(server => {
+            return server.getPrimaryService('heart_rate');
+        }).then(service => {
+            return service.getCharacteristic('heart_rate_measurement');
+        }).then(character => {
+            return character.startNotifications().then(_ => {
+                character.addEventListener('characteristicvaluechanged', (ev) => {
+                    let data = ev.target.value;
+                    let heartRate = data.getUint8(1);
+                    elemHeartRateValue.innerText = heartRate;
+                    chart_data.push(heartRate);
+                    // chart_x.push(chart_data.length);
+                    chart_x.push(`${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()}`);
+                    chart.setOption({
+                        xAxis: {
+                            data: chart_x
+                        },
+                        series: [
+                            {
+                                data: chart_data,
+                            }
+                        ]
+                    });
+                    console.log(`Heart Rate: ${ev.target.value.getInt8(1)}`);
+                });
+            });
+        })
     }
-
-    // navigator.bluetooth.requestDevice({
-    //     filters: [{ services: ['heart_rate'] }]
-    // }).then(device => {
-    //     console.log('device', device);
-    //     return device.gatt.connect();
-    // }).then(server => {
-    //     console.log('server', server);
-    //     return server.getPrimaryService('heart_rate');
-    // }).then(service => {
-    //     console.log('service', service);
-    //     return service.getCharacteristic('heart_rate_measurement');
-    // }).then(characteristic => {
-    //     console.log('characteristic', characteristic);
-    // }).catch(err => { });
 
 });
 
--- src/main.js
+++ src/main.js
@@ -2,11 +2,14 @@ const { app, BrowserWindow, ipcMain } = require('electron')
 const path = require('path')
 
 app.commandLine.appendSwitch('enable-web-bluetooth', true);
+app.commandLine.appendSwitch('enable-experimental-web-platform-features');
 
 const createWindow = () => {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        minWidth: 800,
+        minHeight: 600,
         webPreferences: {
             // preload: path.join(__dirname, 'preload.js'),
             nodeIntegration: true,
@@ -14,7 +17,7 @@ const createWindow = () => {
         },
         frame: true,
         titleBarOverlay: {
-            color: '#F0F2F2',
+            color: '#e4e4e4',
             symbolColor: '#F25E86',
             height: 30
         },
@@ -23,15 +26,15 @@ const createWindow = () => {
 
     win.webContents.on('select-bluetooth-device', (event, deviceList, callback) => {
         event.preventDefault()
-        const scannedDeviceLists = [];
-        deviceList.forEach(device => {
-            if (device.deviceName.indexOf('未知') === -1) scannedDeviceLists.push(device)
-        });
-        console.log('scannedDeviceLists', scannedDeviceLists)
-        win.webContents.send('push-device-list', scannedDeviceLists);
-        if (deviceList && deviceList.length > 0) {
+        console.log(deviceList);
+        if (deviceList && deviceList.length > 0 && deviceList[0].deviceName.indexOf('未知') === -1) {
+            console.log('push');
+            win.webContents.send('push-connect-status', {
+                status: true,
+                device: deviceList[0]
+            })
             callback(deviceList[0].deviceId)
-        } 
+        } else callback('')
     })
 
     win.webContents.openDevTools();
